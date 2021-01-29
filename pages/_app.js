4773c0a506d70a4452019872a8ddd264b43951c1
pedromaranini <pedrolucasmaranini30@gmail.com>
2021-01-29 13:28:53 -0300

    code first

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,30 @@
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import db from '../db.json';
 
 const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+  }
+
   body {
     margin: 0;
     padding: 0;
-    box-sizing: border-box;
+    
+    display: flex;
+    flex-direction: column;
+    font-family: 'Lato', 'sans-serif';
+
+    color: ${({ theme }) => theme.colors.contrastText};
+  }
+
+  html, body {
+    min-height: 100vh;
+  }
+
+  #__next {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
   }
 `
 
@@ -17,8 +37,9 @@ const theme = {
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <GlobalStyle />
+      
       <ThemeProvider theme={theme}>
+        <GlobalStyle />
         <Component {...pageProps} />
       </ThemeProvider>
     </>
